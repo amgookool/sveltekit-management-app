db15dd93dbb566030b195cb86e0de8995b72d022
Adrian Gookool <amgookool@hotmail.com>
2024-05-07 21:30:58 -0400

    added dashboard page layout and started sidebar

---
+++ src/lib/components/layout/Sidebar.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { cn } from "@/utils";
+
+    
+</script>
+
+<h1 class={cn('text-lg')}>Sidebar</h1>
---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+	import Sidebar from '@/components/layout/Sidebar.svelte';
+
+	export let data: LayoutData;
+</script>
+
+<Sidebar />
+<main>
+	<slot />
+</main>
