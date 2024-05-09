90024dff4934e7b2fdefa90c9322981285b68df2
Adrian Gookool <amgookool@hotmail.com>
2024-05-09 14:24:43 -0400

    Added Action to logout user

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,5 +1,18 @@
+import { redirect, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
     return {};
 }) satisfies PageServerLoad;
+
+
+export const actions: Actions = {
+    logout: async (event) => {
+        console.log(event.request.url)
+        console.log("Logout Action called");
+        const refresh_token = event.cookies.get('refresh_token');
+        if (refresh_token)
+            event.cookies.delete('refresh_token', { path: '/' });
+        throw redirect(301,'/login');
+    }
+}
